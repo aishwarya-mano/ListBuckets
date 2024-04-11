09269fb3b55b32ce1c5b14dd03d02c22d02e7459
Aishwarya Manoharan <143560747+aishwarya-mano@users.noreply.github.com>
2024-04-10 21:46:57 -0700

    Update ListBuckets.java

--- testProject/src/main/java/org/example/ListBuckets.java
+++ testProject/src/main/java/org/example/ListBuckets.java
@@ -6,12 +6,31 @@ import software.amazon.awssdk.services.s3.model.Bucket;
 import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
 import java.util.List;
 
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.core.sync.RequestBody;
+
+import java.nio.charset.StandardCharsets;
 public class ListBuckets {
     public static void main(String[] args) {
+        String bucket_name="mytestingbucket94";
+        String object_key="test.txt";
+        String content="Testing the permission";
+
         Region region = Region.US_WEST_2;
         S3Client s3 = S3Client.builder()
                 .region(region)
                 .build();
+        try{
+            PutObjectRequest putObjectRequest=PutObjectRequest.builder().bucket(bucket_name).key(object_key).build();
+            s3.putObject(putObjectRequest, RequestBody.fromString(content, StandardCharsets.UTF_8));
+            System.out.println("Object created successfully");
+
+        }
+        catch (Exception e)
+        {
+            System.out.print(e.getMessage());
+        }
         listAllBuckets(s3);
     }
 
